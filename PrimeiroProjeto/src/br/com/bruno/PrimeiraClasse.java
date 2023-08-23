21e266e62cf5c1bd8eedbaf558eca8071eb83fea
Bruno Tesser <just.brunot@gmail.com>
2023-08-23 17:06:47 -0300

    att PrimeiraClasse.java

--- PrimeiroProjeto/src/br/com/bruno/PrimeiraClasse.java
+++ PrimeiroProjeto/src/br/com/bruno/PrimeiraClasse.java
@@ -3,7 +3,8 @@ package br.com.bruno;
 public class PrimeiraClasse {
 
     public static void main(String args[]){
-        System.out.print("Hello World");
+        System.out.print("Hello World Bruno");
+
     }
 
 
