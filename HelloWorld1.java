ee12ac0ddd160bb09ae8a3037da7431558f6fa6a
Bruno Tesser <just.brunot@gmail.com>
2023-08-23 07:37:28 -0300

    att .java (if, else if, else)

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,7 +1,14 @@
 public class HelloWorld1 {
 
   public static void main(String[] args) {
-    System.out.print("Hello" + args[0]);
-  } 
+    String ambiente = args[0];
+    if (ambiente.equalsIgnoreCase("DEV")) {
+      System.out.print("Executando em DEV");
+    } else if (ambiente.equalsIgnoreCase("TEST")) {
+      System.out.print("Executando em TEST");
+    } else {
+      System.out.print(ambiente);
+    }
+  }  
 }
 
