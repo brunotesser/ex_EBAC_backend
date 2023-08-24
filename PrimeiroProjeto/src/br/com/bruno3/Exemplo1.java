befc95c8944ef5a1e2e5ae8216b3af671f84dee3
Bruno Tesser <just.brunot@gmail.com>
2023-08-24 06:58:51 -0300

    add newclass to boxing and unboxing

---
+++ PrimeiroProjeto/src/br/com/bruno3/Exemplo1.java
@@ -0,0 +1,22 @@
+package br.com.bruno3;
+
+public class Exemplo1 {
+
+    public static void main (String args[]){
+
+        //boxing
+        Boolean status = true;
+        Boolean status1 = Boolean.TRUE;
+
+        Character c = 'A';
+
+        Integer idade = 10;
+        Long cpf = 1234l;
+
+        //unboxing
+        boolean status2 = Boolean.TRUE;
+        char letra = Character.valueOf('A');
+
+        int idade2 = Integer.valueOf(12);
+    }
+}
---
+++ PrimeiroProjeto/src/br/com/bruno3/cliente.java
@@ -0,0 +1,21 @@
+package br.com.bruno3;
+
+public class cliente {
+
+    private Integer codigo;
+
+    private Long codigoMaior=1232l;
+
+    private Double valorDecimal1 = 10.1;
+
+    private Boolean status = false;
+
+    private Float valorDecimal=10.0f;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character letra = 'A';
+
+}
