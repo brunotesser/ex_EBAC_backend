e9b5080ea5532ddc2614458e76a10715d00dbdb8
Bruno Tesser <just.brunot@gmail.com>
2023-08-24 05:59:27 -0300

    add packege br.com.bruno1

---
+++ PrimeiroProjeto/src/br/com/bruno2/Exemplos.java
@@ -0,0 +1,22 @@
+package br.com.bruno2;
+
+public class Exemplos {
+
+    private int codigo;
+
+    private long codigoMaior;
+
+    private float valorDecimal;
+
+    private double valorDecimal1;
+
+    private String texto;
+
+    private boolean status;
+
+    private short shor;
+
+    private byte bi;
+
+
+}
